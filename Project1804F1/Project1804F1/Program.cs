8a6dea96d401506c617436143da0bdcafa2d185a
asp <asp@Aptechnk.edu>
2019-11-06 16:03:57 +0500

    Commited By Shaheer On 6-November-2019

---
+++ Project1804F1/Project1804F1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1804F1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("This Is My First Program On C#");
+
+            Console.ReadLine();
+        }
+    }
+}
